c11e66a9f3be217676cdceb1a5e8d393fcecd912
Mersharch <mersharchdemenya@icloud.com>
2022-04-02 22:59:30 +0000

    edited landing page not fully done but i'd continue later, what i'v done is just a template

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,41 @@
 import '../styles/home.css';
 import Navbar from '../components/Navbar';
+import lunch from '../images/lunch.png'
 
 function Home() {
   return (
-    <div className="homeContent">
-      <Navbar />
-      <div className="homePageInfo">
-        <h2>Sunny-Side</h2>
-        <span>___________O___________</span>
-        <h1>Hotel</h1>
-        <p>ROOMS-SUITE</p>
-      </div>
-      <div className="btns">
-        <button className='btn'>Contact Now</button>
-        <button className='btn'>Reserve Now</button>
-      </div>
+    <div className="home">
+        <section className='landing'>
+            <div className='landing-container'>
+                <Navbar />
+                <div className='landing-text'>
+                    <h2 className='hotel-name'>Sunny-Side</h2>
+                    <span>___________O___________</span>
+                    <h1 className='hotel-name'>Hotel</h1>
+                    <p className='rns'>ROOMS - SUITE</p>
+                </div>
+            </div>
+        </section>
+
+        <section className='about-us'>
+            <div className='au-container'>
+                <img src={lunch} alt='sunnyside' />
+                <div className='au-text'>
+                    <h2 >Our Story</h2>
+                    <p >kjavdvyv
+                    asvjhvbkjbhav
+                    avdskjvchdbsyvckdsyvskh ajvkhkhvvhasvja
+                    dcdcsajcvhduykidbvkyicvdjchvdjkhvh s 
+                    ds djv ms  dskjy vhs kvhsavbkds s,h vitalssdkjhvdhva
+                    asddishksvvds
+                    skhvkdshbvsd,jbk hdvs,jbksdhksvjds
+                    diyskegwkve 
+                     wkhb wantwhwcviwk cwbvw wivvck
+                     wiywe wuiw wvbv
+                     </p>
+                </div>               
+            </div>
+        </section>
     </div>
   );
 }
