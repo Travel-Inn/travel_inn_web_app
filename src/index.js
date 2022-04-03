9a91360896a6fc91ae31acdd2dc2f9da60676ed4
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-04-03 10:15:52 +0000

    Connected firebase to index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "./utils/firebase";
 
 ReactDOM.render(
   <React.StrictMode>
