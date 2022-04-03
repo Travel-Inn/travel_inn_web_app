3ccb744396859512fd1037f4ecdf6dc97d1f8cf3
Mersharch <mersharchdemenya@icloud.com>
2022-04-03 11:44:08 +0000

    started footer

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import '../styles/footer.css'
+
+
+export default function Footer(){
+    return(
+        <footer className='footer'>
+            <div className='footer-container'>
+                
+            </div>
+        </footer>
+    );
+}
