f74f783bf2d17cfaf32b85ffc7a5dd1533bd6f4c
= <=>
2022-04-27 00:32:29 +0000

    add route navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./home";
+import GuestsNavBar from "./components/GuestNavBar/guestsNavBar";
+import Widget from "./components/widget/Widget";
+import AllGuests from "./NavTabs/allguests";
+import TopAndSideBAr from "./topandside";
+
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TopAndSideBAr />} >
+            <Route path="home" element={<Widget />} />
+            <Route path="guests" element ={<GuestsNavBar/>} >
+              <Route path="allguests" element={<AllGuests/>} />
+              <Route path="pending"  />
+              <Route path="booked"  />
+              <Route path="departed" />
+            </Route>
+            <Route path="menu" element ={<TopAndSideBAr />} />
+            <Route path="contact" element ={<TopAndSideBAr />} />
+        </Route>
+      </Routes>
+  </BrowserRouter>
   );
 }
 
