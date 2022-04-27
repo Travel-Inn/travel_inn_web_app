8df6016bdbdca6b50abcba4e2912fce291bbbc98
= <=>
2022-04-27 00:36:03 +0000

    multiple widget content

--- src/components/widget/Widget.js
+++ src/components/widget/Widget.js
@@ -2,9 +2,23 @@ import './Widget.scss';
 
 const Widget = () => {
     return (
-        <div className="widget">
-            <div className="left">left</div>
-            <div className="right">right</div>
+        <div style={{display: 'flex', width: '100%', marginTop: 20}}>
+            <div className="widget">
+                <div className="left">left</div>
+                <div className="right">right</div>
+            </div>
+            <div className="widget">
+                <div className="left">left</div>
+                <div className="right">right</div>
+            </div>
+            <div className="widget">
+                <div className="left">left</div>
+                <div className="right">right</div>
+            </div>
+            <div className="widget">
+                <div className="left">left</div>
+                <div className="right">right</div>
+            </div>
         </div>
     );
 }
