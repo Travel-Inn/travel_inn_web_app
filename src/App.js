28fd8aae589bbdbae2eff0f88d204d71c17e6aef
= <=>
2022-04-28 02:02:40 +0000

    add signup screen to navigation stack

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import GuestsNavBar from "./components/GuestNavBar/guestsNavBar";
 import Widget from "./components/widget/Widget";
 import AllGuests from "./NavTabs/allguests";
 import TopAndSideBAr from "./topandside";
+import {app} from "./utils/config";
+import React, { useEffect } from 'react';
+import SignUp from "./components/auth/signup";
 
 
 function App() {
+  useEffect(() => {
+    console.log(app);
+  }, []);
+
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TopAndSideBAr />} >
+        <Route path="/" element={<SignUp />} />
+        <Route path="begin" element={<TopAndSideBAr />} >
             <Route path="home" element={<Widget />} />
             <Route path="guests" element ={<GuestsNavBar/>} >
               <Route path="allguests" element={<AllGuests/>} />
