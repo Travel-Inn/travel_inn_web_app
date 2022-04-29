77049f1502a3bbe1c1bde6ae9b3bc9943ec55d9a
= <=>
2022-04-29 02:14:03 +0000

    ensure proper nesting of child elements for table

--- src/NavTabs/allguests.js
+++ src/NavTabs/allguests.js
@@ -3,55 +3,58 @@ import './allguests.scss';
 const AllGuests=()=>{
     return(
         <table>
-            <tr>
-                <th>Guest</th>
-                <th>Order Date</th>
-                <th>Check In</th>
-                <th>Check Out</th>
-                <th>Room</th>
-                <th>Status</th>
-            </tr>
-            <tr>
-                <td>Ben</td>
-                <td>{Date().slice(0,15)}</td>
-                <td>{Date().slice(0,15)}</td>
-                <td>{Date().slice(0,15)}</td>
-                <td></td>
-                <td><input type="submit" value="Pending" /></td>
-            </tr> 
-            <tr>
-                <td>Ben</td>
-                <td>{Date().slice(0,15)}</td>
-                <td>{Date().slice(0,15)}</td>
-                <td>{Date().slice(0,15)}</td>
-                <td></td>
-                <td><input type="submit" value="Departed" /></td>
-            </tr>  
-            <tr>
-                <td>Ben</td>
-                <td>{Date().slice(0,15)}</td>
-                <td>{Date().slice(0,15)}</td>
-                <td>{Date().slice(0,15)}</td>
-                <td></td>
-                <td><input type="submit" value="Booked" /></td>
-            </tr>  
-            <tr>
-                <td>Ben</td>
-                <td>{Date().slice(0,15)}</td>
-                <td>{Date().slice(0,15)}</td>
-                <td>{Date().slice(0,15)}</td>
-                <td></td>
-                <td><input type="submit" value="Booked" /></td>
-            </tr>  
-            <tr>
-                <td>Ben</td>
-                <td>{Date().slice(0,15)}</td>
-                <td>{Date().slice(0,15)}</td>
-                <td>{Date().slice(0,15)}</td>
-                <td></td>
-                <td><input type="submit" value="Departed" /></td>
-            </tr> 
-            
+            <thead>
+                <tr>
+                    <th>Guest</th>
+                    <th>Order Date</th>
+                    <th>Check In</th>
+                    <th>Check Out</th>
+                    <th>Room</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Ben</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td></td>
+                    <td><input type="submit" value="Pending" /></td>
+                </tr> 
+                <tr>
+                    <td>Ben</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td></td>
+                    <td><input type="submit" value="Departed" /></td>
+                </tr>  
+                <tr>
+                    <td>Ben</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td></td>
+                    <td><input type="submit" value="Booked" /></td>
+                </tr>  
+                <tr>
+                    <td>Ben</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td></td>
+                    <td><input type="submit" value="Booked" /></td>
+                </tr>  
+                <tr>
+                    <td>Ben</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td>{Date().slice(0,15)}</td>
+                    <td></td>
+                    <td><input type="submit" value="Departed" /></td>
+                </tr> 
+            </tbody>
         </table>
     );
 }
