53194795b7d6f95bed628dec9065d2fb069d994a
Wonder <samuel.nai@yahoo.com>
2022-05-28 20:44:03 +0000

    add information section on profile page

---
+++ src/pages/Profile/myInfo.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './myInfo.scss'
+
+export default function MyInfo(){
+    return(
+        <div className="infoContent">
+           <table>
+               <tbody>
+                   <tr>
+                        <th>First Name:</th><td>Mersharch</td>
+                   </tr>
+                   <tr>
+                        <th>Last Name:</th><td>Wonder</td>
+                   </tr>
+                   <tr>
+                        <th>Email:</th><td>wonersh@307project.com</td>
+                   </tr>
+                   <tr>
+                        <th>Mobile number:</th><td>0551540686</td>
+                   </tr>
+                   <tr>
+                        <th>Gender:</th><td>Male</td>
+                   </tr>
+               </tbody>
+           </table>
+        </div>
+    )
+}
