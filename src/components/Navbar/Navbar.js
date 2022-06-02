4eb4b94e1f85d21adb04f4bf81d06de6047ccd9a
Wonder <samuel.nai@yahoo.com>
2022-06-02 22:14:27 +0000

    recieve and handle user props

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import { NavLink } from 'react-router-dom';
 
 
-function Navbar() {
+function Navbar(props) {
     return (
         <nav>
             <div className="wrapper">
@@ -12,7 +12,7 @@ function Navbar() {
                     <SearchIcon className='icon' />
                 </div>
                 <div className="items">
-                    <span className="nav-name">Wonder</span>
+                    <span className="nav-name">{props.adminName}</span>
                     <NavLink to={'/dashboard/profile'} className="item">
                         <img src="https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWluaW9uc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="avatar" className='avatar' />
                     </NavLink>
